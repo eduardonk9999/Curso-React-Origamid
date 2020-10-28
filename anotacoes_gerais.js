129b63bb1f5a7335a1a8bdd2b6b7e5e8462f0092
Eduardo <thiago.novak7@gmail.com>
2020-10-27 21:06:24 -0300

    Add array

--- anotacoes_gerais.js
+++ anotacoes_gerais.js
@@ -6,22 +6,22 @@ introduzindo elementos como XML que são convertidos em funções de React.
 Casos Especiais
 - class => className
 - for => htmlFor
+*/
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/*
+Arrays
+- Key = É um valor único para cada elemento na array
 
 */
+const App() => {
+    const filmes = ['Before Sunrise', 'Before Sunset'];
+
+    return (
+        <ul>
+            {filmes.map((filmes) => (
+                <li key={filme}>{filme}</li>
+            ))}
+        </ul>    
+    );
+};
