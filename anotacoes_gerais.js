610c2456571d5e6145b5b20c7dfa302e269442cf
eduardonk9999 <thiago.novak7@gmail.com>
2020-11-18 13:16:52 -0300

    Add, useMemo

--- anotacoes_gerais.js
+++ anotacoes_gerais.js
@@ -282,14 +282,25 @@ const App = () => {
  * 
  */
 
+ /*
+ Geralmente em ações como operações matematicas
+ */
+
+
+function operacaoLenta() {
+  let c;
+  for (let i = 0; i < 100000000; i++) {
+    c = i + i / 10;
+  }
+  return c;
+}
+
 const App = () => {
   const [contar, setContar] = React.useState(0);
-  const valor = React.useMemo(() => {
-    const localStorageItem = window.localStorage.getItem('produto');
-    // só será executado uma vez
-    console.log('teste');
-    return localStorageItem;
-  }, []);
-  console.log(valor);
+  const t1 = performance.now();
+  const valor = React.useMemo(() => operacaoLenta(), []);
+
+  console.log(performance.now() - t1);
 
   return <button onClick={() => setContar(contar + 1)}>{valor}</button>;
+};
